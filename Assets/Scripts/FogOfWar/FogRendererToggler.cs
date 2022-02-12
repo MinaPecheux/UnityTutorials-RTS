a912bf7e2d986b4a6170fc8f881d6cec4a7fd7c7
Mina Pêcheux <mina.pecheux@gmail.com>
2022-02-13 00:56:26 +0100

    fix(fog): small cpu usage improvement by using coroutines for fog render toggle

--- Assets/Scripts/FogOfWar/FogRendererToggler.cs
+++ Assets/Scripts/FogOfWar/FogRendererToggler.cs
@@ -1,77 +1,113 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class FogRendererToggler : MonoBehaviour
 {
     public GameObject mesh; // reference to the render you want toggled based on the position of this transform
     [Range(0f, 1f)] public float threshold = 0.1f; //the threshold for when this script considers myRenderer should render
 
-    private Camera _camera; //The Camera using the masked render texture
+    private const float _UPDATE_RATE = 0.2f;
 
+    private Camera _camera; // the camera using the masked render texture
+    private Coroutine _texUpdateCoroutine;
+
+    private FogRendererToggler _mainInstance;
     // made so all instances share the same texture, reducing texture reads
     private static Texture2D _shadowTexture;
     private static Rect _rect;
-    private static bool _isDirty = true;// used so that only one instance will update the RenderTexture per frame
+    private static bool _validTexture = true;
 
     private void Awake()
     {
-        // disable if FOV game parameter is inactive
-        if (!GameManager.instance.gameGlobalParameters.enableFOV) {
+        // disable if:
+        // - FOV game parameter is inactive
+        // - or no mesh is defined
+        if (
+            !GameManager.instance.gameGlobalParameters.enableFOV ||
+            !mesh
+        ) {
             this.enabled = false;
             return;
         }
 
-        // disable if no mesh is defined
-        if (!mesh)
-        {
-            this.enabled = false;
-            return;
-        }
-
-        _camera = GameObject.Find("UnexploredAreasCam").GetComponent<Camera>();
+        // mark the "first" (arbitrary) instance as main
+        if (_mainInstance == null)
+            _mainInstance = this;
     }
 
-    private Color GetColorAtPosition()
+    private void OnEnable()
+    {
+        // only run the texture updates on the main instance
+        if (_mainInstance == this)
+        {
+            _camera = GameObject.Find("UnexploredAreasCam").GetComponent<Camera>();
+            _texUpdateCoroutine = StartCoroutine(_UpdatingShadowTexture());
+        }
+        else
+        {
+            _texUpdateCoroutine = null;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_texUpdateCoroutine != null)
+        {
+            StopCoroutine(_texUpdateCoroutine);
+            _texUpdateCoroutine = null;
+        }    
+    }
+
+    private void LateUpdate()
+    {
+        bool active = _GetColorAtPosition().grayscale >= threshold;
+        if (mesh.activeSelf != active)
+            mesh.SetActive(active);
+    }
+
+    private void _UpdateShadowTexture()
     {
         if (!_camera)
         {
-            // if no camera is referenced script assumes there no fog and will return white (which should show the entity)
-            return Color.white;
+            _validTexture = false;
+            return;
         }
 
         RenderTexture renderTexture = _camera.targetTexture;
         if (!renderTexture)
         {
-            //fallback to Camera's Color
-            return _camera.backgroundColor;
+            _validTexture = false;
+            return;
         }
 
-        if (_shadowTexture == null || renderTexture.width != _rect.width || renderTexture.height != _rect.height)
+        if (
+            _shadowTexture == null ||
+            renderTexture.width != _rect.width ||
+            renderTexture.height != _rect.height
+        )
         {
             _rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
             _shadowTexture = new Texture2D((int)_rect.width, (int)_rect.height, TextureFormat.RGB24, false);
         }
 
-        if (_isDirty)
-        {
-            RenderTexture.active = renderTexture;
-            _shadowTexture.ReadPixels(_rect, 0, 0);
-            RenderTexture.active = null;
-            _isDirty = false;
-        }
+        RenderTexture.active = renderTexture;
+        _shadowTexture.ReadPixels(_rect, 0, 0);
+        RenderTexture.active = null;
+    }
 
-        var pixel = _camera.WorldToScreenPoint(transform.position);
+    private Color _GetColorAtPosition()
+    {
+        if (!_validTexture) return Color.white;
+        Vector3 pixel = _camera.WorldToScreenPoint(transform.position);
         return _shadowTexture.GetPixel((int)pixel.x, (int)pixel.y);
     }
 
-    private void Update()
+    private IEnumerator _UpdatingShadowTexture()
     {
-        _isDirty = true;
-    }
-
-    void LateUpdate()
-    {
-        bool active = GetColorAtPosition().grayscale >= threshold;
-        if (mesh.activeSelf != active)
-            mesh.SetActive(active);
+        while (true)
+        {
+            _UpdateShadowTexture();
+            yield return new WaitForSeconds(_UPDATE_RATE);
+        }
     }
 }
